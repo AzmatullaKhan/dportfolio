4f504cb43aad67dc2791d296f83d1e0cf4901f84
klu_2200031618 <kk258786@gmail.com>
2024-04-14 20:48:24 +0530

    Committing everything in a single commit inorder to deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import { AboutMe } from "./pages/aboutme";
+import { Home } from "./pages/home";
+import './pages/z.css'
+function App() {
+  return (
+    <div>
+      {/* <AboutMe /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
